db40cf6d4683dad2df00a2300c54aa29a9f3dcd7
VladDuncea <vlad_duncea_31@yahoo.com>
2017-01-28 23:22:48 +0200

    update

--- app/src/main/java/com/example/oalex/carnetvirtual/LoginActivity.java
+++ app/src/main/java/com/example/oalex/carnetvirtual/LoginActivity.java
@@ -3,12 +3,20 @@ package com.example.oalex.carnetvirtual;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class LoginActivity extends Activity
@@ -18,6 +26,8 @@ public class LoginActivity extends Activity
 
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
+    private String mEmail;
+    private String mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,7 +39,8 @@ public class LoginActivity extends Activity
 
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         mPasswordView = (EditText) findViewById(R.id.password);
-
+        mEmail = mEmailView.getText().toString();
+        mPassword = mPasswordView.getText().toString();
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -56,26 +67,43 @@ public class LoginActivity extends Activity
     }
 
     private void LogIn() {
-        Boolean is_email_right = false;
-        Boolean is_password_right = false;
 
-        String emailText = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
+        Response.Listener<String> loginListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
+                    boolean is_email_right = jsonResponse.getBoolean("is_email_right");
+                    boolean is_password_right = jsonResponse.getBoolean("is_password_right");
+                    if(success){
 
-        for (int i = 0; i < e_mails.length; i++)
-            if (emailText.equalsIgnoreCase(e_mails[i]))
-            {
-                is_email_right = true;
-                is_password_right = password.equals(passwords[i]);
-                i = e_mails.length;
+                        if (!is_email_right)
+                            Toast.makeText(LoginActivity.this, "Wrong e-mail.", Toast.LENGTH_SHORT).show();
+                        else if (!is_password_right)
+                            Toast.makeText(LoginActivity.this, "Wrong password.", Toast.LENGTH_SHORT).show();
+                        else
+                        {
+                            String STID = jsonResponse.getString("STID");
+                            startActivity(new Intent(LoginActivity.this, Main.class));
+                        }
+
+                    }
+                    else{
+                        AlertDialog.Builder alert = new AlertDialog.Builder(LoginActivity.this);
+                        alert.setMessage("Ups.. S-a intamplat ceva neprevazut").setNegativeButton("1napoi",null).create().show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
             }
+        };
+        Login_Request login_Request = new Login_Request (mEmail,mPassword,loginListener);
+        RequestQueue login_Queue = Volley.newRequestQueue(LoginActivity.this);
+        login_Queue.add(login_Request);
 
-        if (!is_email_right)
-            Toast.makeText(this, "Wrong e-mail.", Toast.LENGTH_SHORT).show();
-        else if (!is_password_right)
-            Toast.makeText(this, "Wrong password.", Toast.LENGTH_SHORT).show();
-        else
-            startActivity(new Intent(this, Main.class));
+
     }
 }
 
