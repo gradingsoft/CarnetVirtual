b31970a7cb07dc724088c602d9e5e98c47caed09
VladDuncea <vlad_duncea_31@yahoo.com>
2017-01-29 00:25:53 +0200

    update

--- app/src/main/java/com/example/oalex/carnetvirtual/LoginActivity.java
+++ app/src/main/java/com/example/oalex/carnetvirtual/LoginActivity.java
@@ -70,8 +70,20 @@ public class LoginActivity extends Activity
                         }
                         else
                         {
-                            String STID = jsonResponse.getString("STID");
-                            //new Student();
+
+                            String Sname = jsonResponse.getString("Sname");
+                            String SAddress = jsonResponse.getString("SAddress");
+                            String SPhone = jsonResponse.getString("SPhone");
+                            String CName = jsonResponse.getString("CName");
+                            String STName = jsonResponse.getString("STName");
+                            String STFirstName = jsonResponse.getString("STFirstName");
+                            String STEmail = jsonResponse.getString("STEmail");
+                            String STSerialNr = jsonResponse.getString("STSerialNr");
+                            //String STCnp = jsonResponse.getString("STCnp");
+                            String STAddress = jsonResponse.getString("SPhone");
+                            String STPhone = jsonResponse.getString("STPhone");
+
+                            new Student(Sname,SAddress,SPhone,CName,STName,STFirstName,null,STEmail,STSerialNr,null,STAddress,STPhone);
                             startActivity(new Intent(LoginActivity.this, Main.class));
                         }
 
