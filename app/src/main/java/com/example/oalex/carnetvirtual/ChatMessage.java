ef810175347f2620ec6bf6de2ef0686ec31b7ee4
Oprea Alexandru <oalexprea@yahoo.com>
2017-01-29 20:02:43 +0200

    calendar + firstpage + main screen

--- app/src/main/java/com/example/oalex/carnetvirtual/ChatMessage.java
+++ app/src/main/java/com/example/oalex/carnetvirtual/ChatMessage.java
@@ -1,10 +1,27 @@
 package com.example.oalex.carnetvirtual;
 
+import java.util.Date;
+
 /**
  * Created by oalex on 2017-01-29.
  */
 
 public class ChatMessage
 {
+    public Date date;
+    public Date expirationDate;
+    public String mesaj;
+    public String autor;
+    public int type;
 
+    public ChatMessage(Date date, Date expirationDate, String mesaj, String autor, int type)
+    {
+        this.date = date;
+        this.expirationDate = expirationDate;
+        this.mesaj = mesaj;
+        this.autor = autor;
+        this.type = type;
+
+        Student.student.chatMessages.add(this);
+    }
 }
