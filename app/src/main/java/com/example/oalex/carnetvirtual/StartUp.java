7d2f5960f6ef057c24c6ee37c4d1138690770f0f
VladDuncea <vlad_duncea_31@yahoo.com>
2017-01-29 02:06:55 +0200

    update

--- app/src/main/java/com/example/oalex/carnetvirtual/StartUp.java
+++ app/src/main/java/com/example/oalex/carnetvirtual/StartUp.java
@@ -1,9 +1,12 @@
 package com.example.oalex.carnetvirtual;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +19,8 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+
 public class StartUp extends AppCompatActivity {
 
     @Override
@@ -25,7 +30,6 @@ public class StartUp extends AppCompatActivity {
 
         LinkButtons();
         Serialization.readSerializable(getApplicationContext());
-
         if(Serialization.serialization!=null)
             LogIn();
     }
@@ -64,6 +68,10 @@ public class StartUp extends AppCompatActivity {
                             String STAddress = jsonResponse.getString("STAddress");
                             String STPhone = jsonResponse.getString("STPhone");
 
+                            String Poza = jsonResponse.getString("Poza");
+                            byte[] byteArray = Poza.getBytes("UTF-16");  //Transforma poza in binar
+                            byte[] data = Base64.decode(byteArray, Base64.DEFAULT); // decodeaza poza cryptata in base 64
+                            Bitmap bm = BitmapFactory.decodeByteArray(data, 0 ,data.length); //transforma in bitmap
                             new Student(SName,SAddress,SPhone,CName,STName,STFirstName,null,mEmail,mPassword,STSerialNr,null,STAddress,STPhone);
                             startActivity(new Intent(StartUp.this, Main.class));
                         }
@@ -74,6 +82,8 @@ public class StartUp extends AppCompatActivity {
                         alert.setMessage("Ups.. S-a intamplat ceva neprevazut").setNegativeButton("Inapoi",null).create().show();
                     }
                 } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
 
@@ -107,8 +117,6 @@ public class StartUp extends AppCompatActivity {
 
     private void SubmitCode()
     {
-        Toast.makeText(getApplicationContext(), Serialization.serialization.email, Toast.LENGTH_LONG).show();
-
         String input_code = ((EditText)findViewById(R.id.code_editText)).getText().toString();
         Response.Listener<String> loginListener = new Response.Listener<String>() {
             @Override
