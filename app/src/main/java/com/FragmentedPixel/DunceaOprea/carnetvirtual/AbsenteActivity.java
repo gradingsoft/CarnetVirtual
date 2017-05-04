86e6b62062e29c1312cad7beb59a44ae490a5fe6
Oprea Alexandru <oalexprea@yahoo.com>
2017-05-05 00:13:07 +0300

    medii_layout

--- app/src/main/java/com/FragmentedPixel/DunceaOprea/carnetvirtual/AbsenteActivity.java
+++ app/src/main/java/com/FragmentedPixel/DunceaOprea/carnetvirtual/AbsenteActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class AbsenteActivity extends AppCompatActivity {
 
@@ -18,6 +19,8 @@ public class AbsenteActivity extends AppCompatActivity {
         for (Presences p: Student.student.presences)
             if(!materii.contains(p.materie))
                 materii.add(p.materie);
+
+        Collections.sort(materii);
 
         ListView absenteMaterii = (ListView) findViewById(R.id.materii_list);
         AbsenteAdapter adapter = new AbsenteAdapter(AbsenteActivity.this, R.layout.absente_adapter_layout, materii);
